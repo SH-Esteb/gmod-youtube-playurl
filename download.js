6a8bde38148da4c10152f535d6cb42036360984b
Esteb <55597307+SH-Esteb@users.noreply.github.com>
2020-06-25 23:23:02 +0200

    Update download.js

--- download.js
+++ download.js
@@ -3,29 +3,49 @@ const { loadavg } = require("os");
 const express = require('express')
 const app = express()
 app.use("/mp3",express.static(__dirname+"/mp3"));
+var XMLHttpRequest = require("xmlhttprequest").XMLHttpRequest;
+const fs = require('fs')
 
 
 app.get("/:id",(req,res)=>{
     let code = req.params.id
-    console.log(code)
+    const path = "/root/Musique/mp3/" + code + ".mp3"
+    console.log(path)
 
     var YD = new YoutubeMp3Downloader({
         "ffmpegPath": "/usr/bin/ffmpeg",
         "outputPath": "/root/Musique/example/mp3",
         "youtubeVideoQuality": "highest",
-        "queueParallelism": 2,
+        "queueParallelism": 20,
         "progressTimeout": 2000
     });
+        
+    try {
+        if (fs.existsSync(path)) {
+            const linker = "http://_______/mp3/" + code + ".mp3"
+            res.send( '{ "link":"' + linker + '"}');
+        }else{
+            YD.download(code, code + ".mp3")
     
-    //Download video and save as MP3 file
-    YD.download(code)
+            YD.on("finished", function(err, data) {
+                    let pata = data.file.replace("/root/Musique/mp3/", "")
+                    data.link = "http://__________/mp3/" + pata
+                    res.send(data)
+                    console.log(pata)
+            });
 
-    YD.on("finished", function(err, data) {
-        let pata = data.file.replace("/root/Musique/mp3/", "")
-        data.link = "http://_______/mp3/" + pata
-        res.send(data)
-        console.log(pata)
-    });
+            YD.on("error", function(error) {
+                res.send(error)
+            });
+
+            YD.on("progress", function(progress) {
+                console.log(JSON.stringify(progress));
+            });
+        }
+      } catch(err) {
+        console.error(err)
+      }
+
 })
 
 app.listen(80)
